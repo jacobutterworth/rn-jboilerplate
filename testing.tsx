1b3c77b8fde73981b4982c65c139519c0b99e470
Jacob Butterworth <jacob.butterworth@gmail.com>
2022-06-02 15:41:12 +0100

    Added my project

---
+++ testing.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeScren = () => {
+  return (
+    <View>
+      <Text>HomeScren</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScren;
